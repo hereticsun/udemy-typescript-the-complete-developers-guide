d70a4c0104e67101d0528de32e75a86621c9ee0f
Azlan <azlan@cuttilan.co.uk>
2025-05-30 05:53:52 +0100

    302 More on State

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,13 +1,15 @@
-import ReactDOM from "react-dom/client";
-import GuestList from './state/GuestList';
-const el = document.getElementById("root");
+import ReactDOM from 'react-dom/client';
+
+import UserSearch from './state/UserSearch';
+
+const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   );
 };
